af459a78a6e63520d68faaec716312cc4fcad2bf
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-03-18 13:20:32 +0530

    Updated Ram dao impl

---
+++ src/main/java/com/repair/mart/core/dao/impl/RamDaoImpl.java
@@ -0,0 +1,33 @@
+package com.repair.mart.core.dao.impl;
+
+import com.repair.mart.core.dao.api.RamDaoApi;
+import com.repair.mart.core.dto.Ram;
+
+import java.util.List;
+
+public class RamDaoImpl implements RamDaoApi {
+    @Override
+    public List<Ram> getAllRams() {
+        return null;
+    }
+
+    @Override
+    public Ram getRamById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Ram createRam(Ram ram) {
+        return null;
+    }
+
+    @Override
+    public Ram updateRam(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteRam(Long id) {
+
+    }
+}
