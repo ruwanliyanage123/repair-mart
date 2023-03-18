e64781cff6409040d3aca202485de2f2976132db
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-03-18 13:22:00 +0530

    Updated Ram service impl

---
+++ src/main/java/com/repair/mart/core/service/impl/RamServiceImpl.java
@@ -0,0 +1,33 @@
+package com.repair.mart.core.service.impl;
+
+import com.repair.mart.core.dto.Ram;
+import com.repair.mart.core.service.api.RamService;
+
+import java.util.List;
+
+public class RamServiceImpl implements RamService {
+    @Override
+    public List<Ram> getAllRams() {
+        return null;
+    }
+
+    @Override
+    public Ram getRamById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Ram createRam(Ram ram) {
+        return null;
+    }
+
+    @Override
+    public Ram updateRam(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteRam(Long id) {
+
+    }
+}
